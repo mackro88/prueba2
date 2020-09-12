1852b8cf88113f9d9755a0bcc72a9b1a852f08ee
Romina Pineda <romi.macka@gmail.com>
2020-09-11 23:19:46 -0300

    ready2

--- web_nasa.rb
+++ web_nasa.rb
@@ -12,18 +12,11 @@ def request(address)
     response = http.request(request)
     return JSON.parse(response.read_body)
 end
-curiosity = request('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=0&api_key=LsR7M0qXQpiolnXxaLYGiKoAgR9ZghBKsrOAqVLn')
-opportunity = request('https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=0&api_key=LsR7M0qXQpiolnXxaLYGiKoAgR9ZghBKsrOAqVLn')
-spirit = request('https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?sol=0&api_key=LsR7M0qXQpiolnXxaLYGiKoAgR9ZghBKsrOAqVLn')
 
-#puts "curiosity"
-#curiosity["photos"].each do |fotos|
-#puts fotos["camera"]
-#end
-#curiosity["photos"].each do |item|
-#    puts "<li><img src='"+item['img_src']+"'></li>" 
-#end
-
+user_key = 'LsR7M0qXQpiolnXxaLYGiKoAgR9ZghBKsrOAqVLn'
+curiosity = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=0&api_key=#{user_key}")
+opportunity = request("https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=1&api_key=#{user_key}")
+spirit = request("https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?sol=2&api_key=#{user_key}")
 
 def head
     '<!doctype html>
@@ -37,35 +30,70 @@ def head
         <!-- CSS only -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
+        <style type="text/css"> ul {list-style-type: none;} </style>"
       </head>
       <body style="background-color:black; color: white">'
 end
 
-def build_web_page(curiosity)
+def build_web_page(curiosity, opportunity, spirit)
     lista = '
-        <div class="content"> 
-            <ul><h1>Curiosity</h1>
-                <div class="row">
-                    '
-    curiosity["photos"].each do |item|
-        img_link = item['img_src'] 
-        lista += "<li><img src='#{img_link}'></li>"
+     <div class="content"> 
+      <ul><h1>Curiosity</h1>
+       <div class="row">
+        '
+    curiosity["photos"].each do |item_c|
+        img_link_c = item_c['img_src'] 
+        lista += "\t<li class='col-2 pt-2'><img src='#{img_link_c}' width=200px></li>\n"
         end 
-    lista += '      </div>
-                </ul>
-            </div>'
+    lista += ' </div>
+        </ul>
+        <ul><h1>Opportunity</h1>
+       <div class="row">
+        '
+    opportunity["photos"].each do |item_o|
+        img_link_o = item_o['img_src'] 
+        lista += "\t<li class='col-2 pt-2'><img src='#{img_link_o}' width=200px></li>\n"
+        end 
+    lista += ' </div>
+        </ul>
+        <ul><h1>Spirit</h1>
+       <div class="row">
+        '
+    spirit["photos"].each do |item_s|
+        img_link_s = item_s['img_src'] 
+        lista += "\t<li class='col-2 pt-2'><img src='#{img_link_s}' width=200px></li>\n"
+        end 
+    lista += ' </div>
+        </ul>
+    </div>
+            '
     return lista
 end
 
 def foot
-    '<!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    '<!-- JS, Popper.js, and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
   </body>
 </html>'
 end
 
-index = head() + build_web_page(curiosity) + foot()
+index = head() + build_web_page(curiosity, opportunity, spirit) + foot()
 File.write('./index.html', index)
+
+
+#def photos_count(curiosity)
+#    n_photos = 0
+#    print "La cámara  tomó #{n_photos}"
+#    hash_camara = {}
+ #   curiosity["photos"].each do |item_c|        
+#        n_photos = item_c["camera"].count
+
+#       hash_camara = nombre["camera"].select{|x,v| x==}
+#    end
+#    print hash_camara
+
+#end
+
+#photos_count(curiosity)
